db6c5e131f9cbd27005fbf760808dffb149a0cb9
kevinjwu30 <54558741+kevinjwu30@users.noreply.github.com>
2019-10-01 18:45:24 -0700

    Update imageblur1.rb

--- imageblur1.rb
+++ imageblur1.rb
@@ -1,20 +1,48 @@
 class Image
 
-	def inititalize (picture)
+	def inititalize picture
 		@picture = picture
 	end
 
-	def output_image
-		@picture.each do |data|
-		puts data.join 
-	end
+def get_ones
+	ones = |iamge|
+	@picture.each_with_index do | row, row_number |
+		row.each_with_index do |col_number|
+			if item == 1
+				ones << [row_number, col_number]
+			end
+		end
+
 	end
 end
 
+def blur
+	ones = get_ones
+
+ 
+
+
+ if row_number == found_row_number && col_number == found_col_number
+              @picture[row_number -1][col_number] = 1 unless row_number == 0 #up
+              @picture[row_number +1][col_number] = 1 unless row_number >= 3 #down
+              @picture[row_number][col_number -1] = 1 unless col_number == 0 #left
+              @picture[row_number][col_number +1] = 1 unless col_number >= 3 #right
+            end
+          end
+        end
+      end
 
 
 
 
+
+	def output_image
+		@picture.each do |image|
+			puts data.join
+	end
+	end
+
+end
 
 image = Image.new([
   [0, 0, 0, 0],
@@ -22,4 +50,11 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
+
+#original output
+image.output_image
+puts
+image.blur!
+
+#blurred output
 image.output_image
